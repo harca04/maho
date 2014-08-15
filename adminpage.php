b9801e116bd03da98fc3525df99deb9c0285407a
harca04 <harca04@gmail.com>
2014-08-16 00:43:53 +0700

    wow
    gembel

---
+++ adminpage.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+</head>
+
+<body>
+<p>Honeyguide Admin Page</p>
+<p>&nbsp;</p>
+<table width="200" border="0">
+  <tr>
+    <td>Insert Product</td>
+    <td><input type="button" name="button" id="button" value="Go"></td>
+  </tr>
+  <tr>
+    <td>Delete Product</td>
+    <td><input type="button" name="button2" id="button2" value="Go"></td>
+  </tr>
+  <tr>
+    <td>View Sales</td>
+    <td><input type="button" name="button3" id="button3" value="Go"></td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+  </tr>
+</table>
+<p>&nbsp;</p>
+</body>
+</html>
