a31f72bd08e93516fdbefeb2479496c034cd2ff6
dinansilmi <mrizal_r@yahoo.com>
2014-08-16 21:15:14 +0700

    solved bray........
    kuncinya nih...SET PASSWORD FOR carihij1@localhost = PASSWORD('dafuq')
    , gue ubah di phpmyadminnya...trus file koneksi.php nya jg dirubah

--- home.php
+++ home.php
@@ -6,6 +6,7 @@
 <head>
 <meta charset="utf-8">
 <title>HONEYGUIDE MAHO SYSTEM</title>
+<link rel="icon" type="image/gif" href="http://hg.carihijab.com/dinan-favicon.ico" />
 <style type="text/css">
 <!--
 .style1 {
@@ -20,6 +21,10 @@
 <p align="center" class="style1">Ahhhh... sukses masuk mass.....cium aku masss... </p>
 <p align="center">
 <img src="http://www.aanyelier.files.wordpress.com/2009/06/olga-syahputra-genit1.jpg" alt="najong"></p>
-<p align="center"><a href="index.php">logout cyinn...</a></p>
+
+<p align="center"><a href="index.php"><input type="submit" value="Logout cyinn..." /></a></p>
+<p align="center">
+<img src="http://37.media.tumblr.com/tumblr_m9trkfsXos1rxlmf0o1_500.gif" alt="dafuq"></p>
+  </tr>
 </body>
 </html>
--- koneksi.php
+++ koneksi.php
@@ -1,7 +1,7 @@
 <?php
 $mysql_hostname = "localhost";
-$mysql_user = "root";
-$mysql_password = "";
+$mysql_user = "carihij1";
+$mysql_password = "dafuq";
 $mysql_database = "carihij1_hg";
 $prefix = "";
 $bd = mysql_connect($mysql_hostname, $mysql_user, $mysql_password) or die("Gak bisa connect database bray!");
